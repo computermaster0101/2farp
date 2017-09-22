f069677276b870e848e1309859dbaf2ac1771a85
computermaster0101 <computermaster0101@rocketmail.com>
2017-09-21 21:01:01 -0600

    using blocking thread as only one app should run at a time

--- src/server.js
+++ src/server.js
@@ -3,12 +3,12 @@ import child_process from 'child_process'
 let subprocess
 
 function spawnAppThread(){
-  child_process.spawn(process.argv[0], ['./biz/Main.js'], {
+  child_process.spawnSync(process.argv[0], ['./biz/Main.js'], {
     stdio: 'inherit'
-  }).on('close', () => {
-    console.log('restarting....')
-    spawnAppThread()
   })
+  console.log('restarting....')
+  spawnAppThread()
+
 }
 
 spawnAppThread()
